ffb81beb9f2f05e0799d705bb0dfe8f20c0b22a1
IveNeSGaming <164744441+IveNeSGaming@users.noreply.github.com>
2024-03-24 17:08:55 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const { Telegraf } = require("telegraf");
 
 // Botni yaratish
-const bot = new Telegraf("token1");
+const bot = new Telegraf("6447415093:AAHiABY5u3Ajl1aI608X4MDgKTP81-8gV9w");
 
 // /start komandasi uchun handler
 bot.command("start", (ctx) => {
